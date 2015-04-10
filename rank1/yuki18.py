94c2ebc2bf73e4fde17352c4ae1df51834c152af
yumechi <yumechi@users.noreply.github.com>
2015-04-10 15:06:48 +0900

    Add Problem 5, 18, 21, 22, 24, 26, 29, 32

--- rank1/yuki18.py
+++ rank1/yuki18.py
@@ -1,1 +1,9 @@
-__author__ = 'owner'
+S = input()
+res = ""
+N = len(S)
+for i in range(1, N + 1):
+    asciic = ord(S[i - 1]) - i
+    while ord("A") > asciic:
+        asciic += 26
+    res += chr(asciic)
+print(res)
--- rank1/yuki5.py
+++ rank1/yuki5.py
@@ -1,1 +1,11 @@
 __author__ = 'owner'
+L = int(input())
+N = int(input())
+Wlist = list(map(int, input().split()))
+Wlist.sort()
+for i in range(N):
+    if sum(Wlist[0:i + 1]) > L:
+        print(i)
+        break
+else:
+    print(N)
