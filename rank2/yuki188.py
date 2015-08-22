60bf48c7d03c066fbf88f60f4b790ac2c7665915
yumechi <yumechi@users.noreply.github.com>
2015-08-22 14:04:23 +0900

    No.183

--- rank2/yuki188.py
+++
@@ -1,7 +0,0 @@
-month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-res = 0
-for i in range(1, 13):
-    for j in range(1, month[i-1] + 1):
-        if i == j // 10 + j % 10:
-            res += 1
-print(res)
