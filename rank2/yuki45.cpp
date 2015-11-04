bc964b32dc0cb3701c995d44ea2f5a17aecf78a4
yumechi <yumechi@users.noreply.github.com>
2015-11-04 10:00:26 +0900

    No.45

---
+++ rank2/yuki45.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <queue>
+
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <functional>
+using namespace std;
+#define FOR(i,a,b) for (int i=(a);i<(b);i++)
+#define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+#define INF 1<<30
+#define ALEN(ARR) (sizeof(ARR) / sizeof((ARR)[0]))
+#define MP make_pair
+#define mp make_pair
+#define pb push_back
+#define PB push_back
+#define DEBUG(x) cout<<#x<<": "<<x<<endl
+#define ll long long
+#define ull unsigned long long
+#define MOD 1000000007
+
+int dp[1010];
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cout.precision(16);
+
+  int N;
+  cin >> N;
+  int V[N];
+  REP(i, N) cin >> V[i];
+
+  dp[0] = V[0];
+  dp[1] = max(V[0], V[1]);
+  FOR(i, 2, N) dp[i] = max(dp[i-1], dp[i-2]+V[i]);
+
+  cout << dp[N-1] << endl;
+  return 0;
+}
