ea6e00071f449dee396add7772b29fe8b95b310e
yumechi <yumechi@users.noreply.github.com>
2016-02-13 04:09:28 +0900

    Fix No.327

--- rank2/yuki327.py
+++ rank2/yuki327.py
@@ -1,21 +1,3 @@
-# please use positive value
-def convertDecimal(
-            num, \
-            decimal, \
-            characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", \
-            lowercharacter=False \
-    ):
-    
-    # decimal only can use 2 to 36
-    if num < 0 and not 2 <= decimal <= 36:
-        return None
-
-    ret = ""
-    while num > 0:
-        ret = characters[num % decimal] + ret
-        num //= decimal
-    return ret
-
 def solve(num):
     words = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     ret = words[num % 26]
