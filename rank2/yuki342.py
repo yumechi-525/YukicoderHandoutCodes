6f5bf76912e2b4d25a9c200a8f3438989f490336
yumechi <yumechi@users.noreply.github.com>
2016-02-21 07:41:16 +0900

    No.342

--- rank2/yuki342.py
+++ rank2/yuki342.py
@@ -1,27 +1,17 @@
 import re
 
 def solve():
-    s = input()
-    if s.count("ｗ") == 0 or s.count("ｗ") == len(s):
-        print("")
-        return True
-
-    s = re.sub(r"^ｗ+", "", s)
+    s = re.sub(r"^ｗ+", "", input())
     if s.count("ｗ") == 0:
         print("")
-        return True
-        
+        exit(0)
+ 
     removeW = re.sub(r"ｗ+", "ｗ", s)
     splitList = removeW.split("ｗ")
     matchwords = re.findall(r"ｗ+", s)
     maxW = max([len(c) for c in matchwords])
-    idxlist = []
-    for i in range(len(matchwords)):
-        if len(matchwords[i]) == maxW:
-            idxlist.append(i)
-    idxlist.sort()
+    idxlist = [i for i in range(len(matchwords)) if len(matchwords[i]) == maxW]
     print("\n".join([splitList[i] for i in idxlist]))
-    return True
 
 if __name__=="__main__":
     solve()
