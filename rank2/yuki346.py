20177dad2e12d1df31f0a95a551b8139ac59059b
yumechi <yumechi@users.noreply.github.com>
2016-03-09 00:06:32 +0900

    No.346 (test program remove)

--- rank2/yuki346.py
+++ rank2/yuki346.py
@@ -2,7 +2,7 @@ def solve(s):
     # s = input()[::-1]
     s = s[::-1]
     wc = 0
-    
+
     res = 0
     for c in s:
         if c == "c":
@@ -11,19 +11,21 @@ def solve(s):
             continue
         if c == "w":
             wc += 1
-    print(s.count("c"), res) 
     return res
 
-if __name__=="__main__":
-    # solve()
+def test():
     mxnum = 0
     idx = 0
     for i in range(0, 100001):
         r = solve("c" * i + "w" * (100000 - i))
+        print(i, r)
         if r > mxnum:
             mxnum = r
             idx = i
     print("\n" * 3)
     print("***最大ケース***")
     print("idx = %d, mxnum = %d" % (idx, mxnum))
+
+if __name__=="__main__":
+    solve(input())
     
