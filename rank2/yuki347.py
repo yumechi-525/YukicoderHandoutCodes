e88ddbb90157f4b1b44ec3fdfdb8112a76bc819f
yumechi <yumechi@users.noreply.github.com>
2016-03-09 02:40:10 +0900

    No.347

--- rank2/yuki347.py
+++ rank2/yuki347.py
@@ -2,21 +2,23 @@ from math import log
 
 def solve():
     n, b = int(input()), int(input())
-    data = [float(f) for f in input().split()]
+    data = input().split()
     res1, res2 = 0.0, 0.0
     
-    for f in data:
-        if f == 0.0:
+    for d in data:
+        if d == "0.0":
             continue
+        f = float(d)
         f1 = f - 1
         res1 += f * (b ** f1)
     print(res1)
     
-    for f in data:
-        f1 = f + 1
-        if f == -1.0:
+    for d in data:
+        if d == "-1.0":
             res2 += log(b)
         else:
+            f = float(d)
+            f1 = f + 1
             res2 += (1 / f1) * (b ** (f + 1))
     print(res2)
 
