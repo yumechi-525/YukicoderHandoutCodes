67383df2776bcffa171b43612a04bf6d852fd21e
yumechi <yumechi0525@gmail.com>
2017-06-18 17:04:52 +0900

    No.518 another pattern

--- rank2/yuki518.py
+++ rank2/yuki518.py
@@ -1,26 +1,26 @@
 def roman2arabic(roman: str) -> int:
-    special_case = { "IV" : 4, "IX" : 9, "XL" : 40, "XC" : 90, "CD" : 400, "CM" : 900 }
-    normal_case = { "I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000 }
+    special_case = [["IV", 4], ["IX", 9], ["XL", 40], ["XC", 90], ["CD", 400], ["CM", 900]]
+    normal_case = [["I", 1], ["V", 5], ["X", 10], ["L", 50], ["C", 100], ["D", 500], ["M", 1000]]
     arabic = 0
-    for s in special_case.keys():
-        if s in roman:
-            arabic += special_case[s]
-            roman = roman.replace(s, "")
-    for n in normal_case.keys():
-        if n in roman:
-            arabic += roman.count(n) * normal_case[n]
+    for s in special_case:
+        if s[0] in roman:
+            arabic += s[1]
+            roman = roman.replace(s[0], "")
+    for n in normal_case:
+        if n[0] in roman:
+            arabic += roman.count(n[0]) * n[1]
     return arabic
 
 def arabic2roman(arabic: int) -> str:
     if arabic > 3999:
         return "ERROR"
     roman = ""
-    roman_numerics = { 1000 : "M", 900 : "CM", 500 : "D", 400 : "CD", 100 : "C", 90 : "XC", 50 : "L", 40 : "XL", 10 : "X", 9 : "IX", 5 : "V", 4 : "IV", 1 : "I" }
+    roman_numerics = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
     while arabic > 0:
-        for n in roman_numerics.keys():
-            if arabic >= n:
-                roman += roman_numerics[n]
-                arabic -= n
+        for n in roman_numerics:
+            if arabic >= n[0]:
+                roman += n[1]
+                arabic -= n[0]
                 break
     return roman 
 
